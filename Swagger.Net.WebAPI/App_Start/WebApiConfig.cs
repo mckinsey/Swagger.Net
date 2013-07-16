06d4093f0881ab9aee0fc5691385992935ea8579
Steve Jansen <steve_jansen@mckinsey.com>
2013-07-15 21:18:29 -0400

    adding a Pet controller for defect repro cases

--- Swagger.Net.WebAPI/App_Start/WebApiConfig.cs
+++ Swagger.Net.WebAPI/App_Start/WebApiConfig.cs
@@ -7,9 +7,21 @@ namespace Swagger.Net.WebApi
 {
     public static class WebApiConfig
     {
-        public static void Register(HttpConfiguration config)
+        public static void Register(HttpConfiguration configuration)
         {
-            config.Routes.MapHttpRoute(
+            configuration.Routes.MapHttpRoute(
+                name: "Custom route for linking a pet to a user",
+                routeTemplate: "api/Pet/{id}/User/{userId}",
+                defaults: new { controller="Pet", action="PutUser", userId = RouteParameter.Optional },
+                constraints: new { id = @"\d+", userId = @"\d*" });
+
+            configuration.Routes.MapHttpRoute(
+                name: "Controller Actions That Are Not HTTP Verbs",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional },
+                constraints: new { action = @"\D+", id = @"\d*", });
+
+            configuration.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
