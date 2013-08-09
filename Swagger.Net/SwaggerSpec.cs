2e96a1c07b5d4fdeaef430f9d95f5481a1cbf615
BSick7 <brad.sickles@gmail.com>
2013-08-09 10:44:34 -0400

    Moved data type name conversions into spec file to be used for any type of specification matching.  Now showing proper primitive types for containers and system types.  Added unit tests to verify this and nullable types.

---
+++ Swagger.Net/SwaggerSpec.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Swagger.Net
+{
+    public class SwaggerSpec
+    {
+        private static readonly Dictionary<Type, string> SYSTEM_TYPE_NAMES = new Dictionary<Type, string>
+        {
+            { typeof(bool), "boolean" },
+            { typeof(sbyte), "byte" },
+            { typeof(byte), "byte" },
+            { typeof(ushort), "int" },
+            { typeof(short), "int" },
+            { typeof(uint), "int" },
+            { typeof(int), "int" },
+            { typeof(ulong), "long" },
+            { typeof(long), "long" },
+            { typeof(float), "float" },
+            { typeof(double), "double" },
+            { typeof(decimal), "double" },
+            { typeof(char), "string" },
+            { typeof(string), "string" },
+            { typeof(DateTime), "Date" },
+            { typeof(TimeSpan), "Date" },
+            { typeof(object), "object" },
+        };
+
+        public static string GetDataTypeName(Type type)
+        {
+            if (type == typeof(string)) //guard clause because string is also IEnumerable
+                return GetNameFromSimpleType(type);
+
+            if (type == typeof(IEnumerable) || type.GetInterfaces().Any(t => t == typeof(IEnumerable)))
+            {
+                var typeArg = type.IsGenericType ? type.GetGenericArguments().First() : typeof(object);
+                return string.Format("Array<{0}>", GetDataTypeName(typeArg));
+            }
+
+            if (type.IsGenericType)
+            {
+                var genericType = type.GetGenericTypeDefinition();
+                if (genericType == typeof(Nullable<>))
+                    return string.Format("{0}?", GetDataTypeName(type.GetGenericArguments().First()));
+
+            }
+            return GetNameFromSimpleType(type);
+        }
+
+        private static string GetNameFromSimpleType(Type type)
+        {
+            if (SYSTEM_TYPE_NAMES.ContainsKey(type))
+                return SYSTEM_TYPE_NAMES[type];
+            return type.Name;
+        }
+    }
+}
